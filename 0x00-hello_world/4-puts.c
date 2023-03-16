ea0f4d9bbcdb748b917e9f4088b1617fb9180ad4
Manue-7 <emmanualmoyo@gmail.com>
2023-03-16 14:59:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	puts("Line 2\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Line 2\n");
+	return (0);
 }
