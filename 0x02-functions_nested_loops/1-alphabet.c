7c2de6527830118a5d122a9fa1183cab7b24fa9a
Manue-7 <emmanualmoyo@gmail.com>
2023-03-21 20:34:59 -0700

    fix task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * void -Entry point
- * Program that prints the alphabet
+ * print_alphabet - Program that prints the alphabet
  */
 void print_alphabet(void)
 {
 	char alpha;
-	alpha='a';
-	while(alpha<='z')
+	alpha = 'a';
+
+	while (alpha <= 'z')
 	{
 		_putchar(alpha);
 		alpha++;
